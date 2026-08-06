dceb48e5a34222323f46358cf0587ea5fab7a26f
Lin-Jiong-HDU <linjiong2020@outlook.com>
2026-08-06 22:50:41 +0300

    Add AlienGo adaptation for Walk These Ways
    - Added AlienGo config (PD control, standing height 0.35m, stiffness 50, damping 2.0)
    - Added AlienGo URDF and mesh resources
    - Created train_aliengo.py training script (flat terrain, 15-dim gait commands)
    - Adapted from Go1 config: updated body dimensions, PD gains, foot names

---
+++ go1_gym/envs/aliengo/aliengo_config.py
@@ -0,0 +1,102 @@
+from typing import Union
+
+from params_proto import Meta
+
+from go1_gym.envs.base.legged_robot_config import Cfg
+
+
+def config_aliengo(Cnfg: Union[Cfg, Meta]):
+    """Configure WTW for Unitree AlienGo robot."""
+
+    _ = Cnfg.init_state
+    _.pos = [0.0, 0.0, 0.42]  # x,y,z [m] — AlienGo standing height
+    _.default_joint_angles = {
+        'FL_hip_joint': 0.1,
+        'RL_hip_joint': 0.1,
+        'FR_hip_joint': -0.1,
+        'RR_hip_joint': -0.1,
+
+        'FL_thigh_joint': 0.8,
+        'RL_thigh_joint': 1.0,
+        'FR_thigh_joint': 0.8,
+        'RR_thigh_joint': 1.0,
+
+        'FL_calf_joint': -1.5,
+        'RL_calf_joint': -1.5,
+        'FR_calf_joint': -1.5,
+        'RR_calf_joint': -1.5,
+    }
+
+    _ = Cnfg.control
+    _.control_type = 'P'                 # start with simple PD, not actuator_net
+    _.stiffness = {'joint': 50.}         # [N*m/rad] — higher than Go1 (AlienGo is heavier)
+    _.damping = {'joint': 2.0}           # [N*m*s/rad]
+    _.action_scale = 0.25
+    _.hip_scale_reduction = 0.5
+    _.decimation = 4
+
+    _ = Cnfg.asset
+    _.file = '{MINI_GYM_ROOT_DIR}/resources/robots/aliengo/urdf/aliengo.urdf'
+    _.foot_name = "foot"
+    _.penalize_contacts_on = ["thigh", "calf"]
+    _.terminate_after_contacts_on = ["base"]
+    _.self_collisions = 1               # 1 to disable
+    _.flip_visual_attachments = False
+    _.fix_base_link = False
+
+    _ = Cnfg.rewards
+    _.soft_dof_pos_limit = 0.9
+    _.base_height_target = 0.35          # AlienGo standing height target
+
+    _ = Cnfg.reward_scales
+    _.torques = -0.0002
+    _.action_rate = -0.01
+    _.dof_pos_limits = -10.0
+    _.orientation = -5.
+    _.base_height = -30.
+
+    _ = Cnfg.terrain
+    _.mesh_type = 'plane'                # Start with flat terrain
+    _.measure_heights = False
+    _.terrain_noise_magnitude = 0.0
+    _.teleport_robots = True
+    _.border_size = 50
+    _.terrain_proportions = [0, 0, 0, 0, 0, 0, 0, 0, 1.0]
+    _.curriculum = False
+
+    _ = Cnfg.env
+    _.num_observations = 42
+    _.observe_vel = False
+    _.num_envs = 4096
+
+    _ = Cnfg.commands
+    _.lin_vel_x = [-1.0, 1.0]
+    _.lin_vel_y = [-1.0, 1.0]
+    _.heading_command = False
+    _.resampling_time = 10.0
+    _.command_curriculum = True
+    _.num_lin_vel_bins = 30
+    _.num_ang_vel_bins = 30
+    _.lin_vel_x = [-0.6, 0.6]
+    _.lin_vel_y = [-0.6, 0.6]
+    _.ang_vel_yaw = [-1, 1]
+
+    _ = Cnfg.domain_rand
+    _.randomize_base_mass = True
+    _.added_mass_range = [-1, 3]
+    _.push_robots = False
+    _.max_push_vel_xy = 0.5
+    _.randomize_friction = True
+    _.friction_range = [0.05, 4.5]
+    _.randomize_restitution = True
+    _.restitution_range = [0.0, 1.0]
+    _.restitution = 0.5
+    _.randomize_com_displacement = True
+    _.com_displacement_range = [-0.1, 0.1]
+    _.randomize_motor_strength = True
+    _.motor_strength_range = [0.9, 1.1]
+    _.randomize_Kp_factor = False
+    _.Kp_factor_range = [0.8, 1.3]
+    _.randomize_Kd_factor = False
+    _.Kd_factor_range = [0.5, 1.5]
+    _.rand_interval_s = 6
